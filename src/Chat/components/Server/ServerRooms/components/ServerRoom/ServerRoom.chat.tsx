ad3e2ecb8c502ed6db47d09244f0d487b0c64b96
Ammar <entar99ammar@gmail.com>
2022-11-19 04:46:33 +0200

    init

--- src/Chat/components/Server/ServerRooms/components/ServerRoom/ServerRoom.chat.tsx
+++ src/Chat/components/Server/ServerRooms/components/ServerRoom/ServerRoom.chat.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {CiHashtag} from "react-icons/all";
 interface Props{
     title:string,
     href:string
@@ -8,7 +9,7 @@ const ServerRoomChat :React.FC<Props> = ({title,href}) => {
     return (
         <li className="text-base-content px-2 text-opacity-50 hover:text-opacity-100 hover:bg-base-200">
             <Link to={href} className="flex items-center">
-                <span className="text-xl">#</span> <span className="ml-2">{title}</span>
+                <span className="text-md"><CiHashtag/></span> <span className="ml-2">{title}</span>
             </Link>
         </li>
     );
